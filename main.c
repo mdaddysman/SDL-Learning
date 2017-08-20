e01beee79ce70012e256aa6de200626cdef59d1d
Matthew <mdaddysman@gmail.com>
2017-08-20 14:42:14 -0500

    control demo

--- main.c
+++ main.c
@@ -6,14 +6,23 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+void DrawBox(SDL_Renderer *r, SDL_Rect *box); 
+
 int main(int argc, char* args[])
 {
 	SDL_Window *window = NULL;
 	SDL_Surface *screenSurface = NULL;
+	SDL_Renderer *renderer = NULL;
 	int quit = 0;
 	SDL_Event e;
 	int color = 1;
 	time_t t;
+	SDL_Rect boxobj = {
+		200,
+		200,
+		10,
+		10,
+	};
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
@@ -32,70 +41,62 @@ int main(int argc, char* args[])
 		return(0);
 	}
 
-	srand((unsigned)time(&t));
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == NULL)
+	{
+		printf("Renderer could not be created! Error: %s\n", SDL_GetError());
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return(0);
+	}
 
-	screenSurface = SDL_GetWindowSurface(window);
-	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 200, 30));
-	SDL_UpdateWindowSurface(window);
+	srand((unsigned)time(&t));
+	boxobj.x = rand() % (SCREEN_WIDTH - 11) + 1;
+	boxobj.y = rand() % (SCREEN_HEIGHT - 11) + 1;
+	DrawBox(renderer, &boxobj);
 
 	while (quit == 0)
-	{
-		color = rand() % 13;
-		switch (color)
-		{
-		default:
-		case 0:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 0, 0));	
-			break;
-		case 1:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 127, 0));
-			break;
-		case 2:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 255, 0));
-			break;
-		case 3:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 127, 255, 0));
-			break;
-		case 4:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 255, 0));
-			break;
-		case 5:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 255, 127));
-			break;
-		case 6:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 255, 255));
-			break;
-		case 7:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 127, 255));
-			break;
-		case 8:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 0, 255));
-			break;
-		case 9:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 127, 0, 255));
-			break;
-		case 10:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 127, 0, 255));
-			break;
-		case 11:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 0, 255));
-			break;
-		case 12:
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 255, 0, 127));
-			break;
-		}
-		SDL_UpdateWindowSurface(window);
-		//SDL_Delay(50);
+	{		
 		while (SDL_PollEvent(&e) != 0)
 		{
 			if (e.type == SDL_QUIT)
 				quit = 1;
 		}	
+		const Uint8 *currentKeyStates = SDL_GetKeyboardState(NULL);
+		if (currentKeyStates[SDL_SCANCODE_UP] && !currentKeyStates[SDL_SCANCODE_DOWN])
+			boxobj.y--;
+		else if (currentKeyStates[SDL_SCANCODE_DOWN] && !currentKeyStates[SDL_SCANCODE_UP])
+			boxobj.y++;
+		if (currentKeyStates[SDL_SCANCODE_LEFT] && !currentKeyStates[SDL_SCANCODE_RIGHT])
+			boxobj.x--;
+		else if (currentKeyStates[SDL_SCANCODE_RIGHT] && !currentKeyStates[SDL_SCANCODE_LEFT])
+			boxobj.x++;
+
+		if (boxobj.x < 1)
+			boxobj.x = 1;
+		else if (boxobj.x >(SCREEN_WIDTH - 11))
+			boxobj.x = SCREEN_WIDTH - 11;
+
+		if (boxobj.y < 1)
+			boxobj.y = 1;
+		else if (boxobj.y >(SCREEN_HEIGHT - 11))
+			boxobj.y = SCREEN_HEIGHT - 11;
 		
+		DrawBox(renderer, &boxobj);
+		SDL_Delay(15);
 	}
 
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-
 	return(0);
+}
+
+void DrawBox(SDL_Renderer *r, SDL_Rect *box)
+{
+	SDL_SetRenderDrawColor(r, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(r);
+	SDL_SetRenderDrawColor(r, 255, 255, 255, SDL_ALPHA_OPAQUE);
+	SDL_RenderFillRect(r, box);
+	SDL_RenderPresent(r);
 }
